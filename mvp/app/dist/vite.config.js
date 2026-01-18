c946a8b9edf3b4f94efd5b0bd406597c1bd08734
ORDENp <tvoy1000@gmail.com>
2026-01-18 15:57:49 +0300

    2 v.

---
+++ mvp/app/dist/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+// vite.config.js
+export default {
+  base: '/-Pygmalion-/mvp/app/dist/',
+  // остальная конфигурация...
+}
